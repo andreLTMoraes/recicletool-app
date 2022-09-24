fd5d6cc05802ed83ada2b8b1da7a5f02961f8ac2
André Moraes <andre-altm@hotmail.com>
2022-09-24 15:04:33 -0300

    Modal box

---
+++ src/components/Modal.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import { View, ActivityIndicator, Text, TouchableOpacity, TouchableWithoutFeedback} from 'react-native'
+
+import { COLORS } from '../utils/AppStyles'
+
+export default function Modal({
+    title = 'Modal Box',
+    message = 'Modal message',
+    okFunction = () => {}
+}) {
+    return(
+        <View style={{
+            width: '100%',
+            height: '100%',
+            backgroundColor: '#00000099',
+            alignItems: 'center',
+            justifyContent: 'center',
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            zIndex: 10
+        }}>
+            <View style={{
+                width: "80%",
+                minHeight: 200,
+                backgroundColor: '#fff',
+                borderRadius: 25,
+                alignItems: 'center',
+                justifyContent: 'space-between'
+            }}>
+                <Text style={{
+                    color: COLORS.green,
+                    fontWeight: 'bold',
+                    fontSize: 16,
+                    marginTop: 18
+                }}>{title}</Text>
+
+                <Text style={{marginHorizontal: 20}}>{message}</Text>
+
+                <TouchableOpacity
+                    style={{
+                        borderTopWidth: 1,
+                        borderTopColor: '#c6c6c6',
+                        width: '100%',
+                        height: 50,
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                    }}
+                    onPress={() => okFunction()}
+                >
+                    <Text style={{
+                        color: COLORS.green,
+                        fontWeight: 'bold',
+                        fontSize: 16
+                    }}>Ok</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+    )
+}
--- src/contexts/Alerts.js
+++ src/contexts/Alerts.js
@@ -1,11 +1,18 @@
 import React, { createContext, useState } from 'react'
 
 import Loading from '../components/Loading'
+import Modal from '../components/Modal'
 
 export const AlertContext = createContext({})
 
 function AlertProvider({children}) {
     const [loading, setLoading] = useState(false)
+    const [modal, setModal] = useState({
+        title: '',
+        message: '',
+        okFunction: () => setModal({...modal, visible: false}),
+        visible: false
+    })
 
     function showLoading() {
         setLoading(true)
@@ -18,15 +25,26 @@ function AlertProvider({children}) {
         setLoading(false)
     }
 
+    function openModal(title, message) {
+        setModal({
+            ...modal,
+            title,
+            message,
+            visible: true
+        })
+    }
+
     return(
         <AlertContext.Provider 
             value={{
                 showLoading,
-                hideLoading
+                hideLoading,
+                openModal
             }}
         >
             {children}
             {loading && <Loading/>}
+            {modal.visible && <Modal title={modal.title} message={modal.message} okFunction={modal.okFunction}/>}
         </AlertContext.Provider>
     )
 }
