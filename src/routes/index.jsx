d1ef13c0ba5128622518b10f1281318138cdf757
André Moraes <andre-altm@hotmail.com>
2022-11-04 23:49:28 -0300

    Pages

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,9 +2,9 @@ import React, { useContext } from 'react'
 
 import { AuthContext } from '../contexts/Authentication'
 import OutArea from './outArea'
-import Home from '../pages/home'
+import InArea from './inArea'
 
 export default function Routes() {
     const { user } = useContext(AuthContext)
-    return (!!user ? <Home/> : <OutArea/>)
+    return (!!user ? <InArea/> : <OutArea/>)
 }
