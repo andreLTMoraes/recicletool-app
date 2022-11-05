b94542d7a807bd06b2f595699309bcd23feb61b3
André Moraes <andre-altm@hotmail.com>
2022-11-04 23:48:04 -0300

    Some configurations

--- App.js
+++ App.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
 
 import Routes from './src/routes';
 import AlertProvider from './src/contexts/Alerts';
 import AuthProvider from './src/contexts/Authentication';
+
+import InArea from './src/routes/inArea';
 
 export default function App() {
   return (
     <NavigationContainer>
       <AlertProvider>
         <AuthProvider>
-          <Routes/>
+          <InArea/>
         </AuthProvider>
       </AlertProvider>
     </NavigationContainer>
