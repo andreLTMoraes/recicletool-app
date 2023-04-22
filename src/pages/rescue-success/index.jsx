384c5b671a364ca58898d8b1d9e9290a6acfca40
Thales Bezerra <thalesoliveira.bezerra@gmail.com>
2023-04-22 17:38:51 -0300

    Tela de erro no resgate do cupon

--- src/pages/rescue-success/index.jsx
+++ src/pages/rescue-success/index.jsx
@@ -1,19 +1,61 @@
 import { View, Image, Text, StyleSheet, ScrollView, Pressable } from "react-native";
+import { useState } from "react";
 
-export default function RescueSuccess({navigation}) {
+export default function RescueResult({ navigation }) {
+
+    const [success, setSuccess] = useState(false);
+
+    return (
+        <View style={styles.mainContainer}>
+            {
+                success ?
+                    <RescueSuccess navigation={navigation}/> : <RescueError navigation={navigation}/>
+            }
+        </View>
+    );
+}
+
+function RescueSuccess({ navigation }) {
     return (
         <ScrollView style={{ flex: 1, width: '100%' }} contentContainerStyle={{ flexGrow: 1 }}>
             <View style={styles.mainContainer}>
-                <View style = {styles.container}></View>
+                <View style={styles.container}></View>
                 <View style={[styles.container, { flex: 4 }]}>
                     <Image source={require("../../assets/success-icon.png")} style={styles.successIcon} />
                     <Text style={styles.title}>Sucesso!</Text>
                     <Text style={[styles.text, { width: '60%' }]}>Seu cupom foi para seu histórico. O próximo passo é ir a um dos nosso supermercados parceiros e retirar seu kit com um de nossos atendente.</Text>
-                    <View style = {{alignItems: 'center'}}>
+                    <View style={{ alignItems: 'center' }}>
                         <Pressable onPress={() => navigation.navigate('order-history')}>
                             <Text style={styles.textButton}>HISTÓRICO DE CUPONS</Text>
                         </Pressable>
-                        <Pressable style = {{marginTop: 20}} onPress={() => navigation.navigate('home')}>
+                        <Pressable style={{ marginTop: 20 }} onPress={() => navigation.navigate('home')}>
+                            <Text style={styles.textButton}>VOLTAR</Text>
+                        </Pressable>
+                    </View>
+
+                </View>
+            </View>
+        </ScrollView>
+    );
+}
+
+function RescueError({ navigation }) {
+    return (
+        <ScrollView style={{ flex: 1, width: '100%' }} contentContainerStyle={{ flexGrow: 1 }}>
+            <View style={styles.mainContainer}>
+                <View style={styles.container}></View>
+                <View style={[styles.container, { flex: 4 }]}>
+                    <Image source={require("../../assets/error-icon.png")} style={styles.successIcon} />
+                    <Text style={styles.title}>Oops! Algo deu errado.</Text>
+                    <Text style={[styles.text, { width: '60%' }]}>Falha ao gerar cupom.</Text>
+                    <View style={{ alignItems: 'center', width: '100%' }}>
+                        <Pressable style = {styles.button}>
+                            <Text style={[styles.textButton, {color: 'white'}]}>TENTAR NOVAMENTE</Text>
+                        </Pressable>
+                        <Pressable style={{ marginTop: 40 }}>
+                            <Text style={styles.textButton}>DÚVIDAS?</Text>
+                        </Pressable> 
+                        <Pressable style={{ marginTop: 40 }} onPress={() => navigation.navigate('home')}>
                             <Text style={styles.textButton}>VOLTAR</Text>
                         </Pressable>
                     </View>
@@ -68,6 +110,15 @@ const styles = StyleSheet.create({
         fontWeight: '700',
         color: '#4ea674',
         letterSpacing: 1
-    }
+    },
+
+    button: {
+        width: '60%',
+        paddingVertical: 15,
+        backgroundColor: '#4ea674',
+        borderRadius: 30,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
 
 });
